6913b2efc5a633c1d11162c5be3db37c6afbd12f
Emmanuel <j.emmanuel.martinez.e@hotmail.com>
2020-05-27 11:11:16 -0500

    MenúSlider&Modal: Configuración inicial WebPack y SASS

---
+++ README.md
@@ -0,0 +1,14 @@
+ # Menú Slider & Modal
+
+ "Menú Slider & Modal"
+
+ ### Notas:
+ Recuerden reconstruir los módulos de Node
+ ```
+ npm install
+ ```
+
+ Y para construir el build, recuerden:
+ ```
+ npm run build
+ ```
---
+++ src/js/componentes.js
@@ -0,0 +1,14 @@
+// import './../css/componentes.css';
+
+const eventos = () => {
+    console.log('Event Listeners');
+};
+
+const init = () => {
+    console.log('Menú Slider & Modal');
+    eventos();
+};
+
+export {
+    init
+} 

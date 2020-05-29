c7d8eaeacd9ad17f79038307b15c8389921354a5
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2020-05-28 21:53:13 -0500

    Header & Modal Styling, Menú & Modal Toggle

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,7 +1,47 @@
+import {
+    checkEmail,
+    checkRequired,
+    checkLength,
+    checkPasswordMatch
+} from './helper';
 import './../css/componentes.css';
 
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
 const eventos = () => {
-    console.log('Event Listeners');
+
+    toggle.addEventListener('click', () => { // Toggle nav
+        document.body.classList.toggle('show-nav');
+    });
+    open.addEventListener('click', () => { // Show modal
+        modal.classList.add('show-modal');
+    });
+    close.addEventListener('click', () => { // Hide modal 
+        modal.classList.remove('show-modal');
+    });
+    window.addEventListener('click', (e) => { // Hide modal on outside click
+        e.target == modal ? modal.classList.remove('show-modal') : false;
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        console.log('submit');
+        checkRequired([name, email, password, password2]);
+        checkLength(name, 3, 15);
+        checkLength(password, 6, 25);
+        checkEmail(email);
+        checkPasswordMatch(password, password2);
+    });
+
 };
 
 const init = () => {
--- src/js/helper.js
+++ src/js/helper.js
@@ -1,3 +1,62 @@
-/* ****************************** NAME ****************************** */
-/* ****************************************************************** */
-export {}
+// *************** Error Message *************** //
+const showError = (input, message) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+};
+// *************** Success Message *************** //
+const showSuccess = (input) => {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+};
+// *************** Valid Email *************** //
+const checkEmail = (input) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // return re.test(String(email).toLowerCase()); // with @ true, without @ false
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+};
+// *************** Check Required *************** //
+const checkRequired = (inputArray) => {
+    inputArray.forEach((input) => {
+        if (input.value.trim() === '') {
+            showError(input, `${getFileName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+};
+// *************** Check input length *************** //
+const checkLength = (input, min, max) => {
+    if (input.value.length < min) {
+        showError(input, `${getFileName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFileName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+};
+// *************** Password Match *************** //
+const checkPasswordMatch = (input1, input2) => {
+    if (input1.value !== input2.value) {
+        showError(input2, 'Passwords do not match');
+    }
+};
+// *************** Get File Name *************** //
+const getFileName = (input) => {
+    const mayuscula = input.id.charAt(0).toUpperCase();
+    const restoPalabra = input.id.slice(1);
+
+    return mayuscula + restoPalabra;
+};
+
+export {
+    checkEmail,
+    checkRequired,
+    checkLength,
+    checkPasswordMatch
+}
